0491f51663942b53867aa09c6e2798de5a926935
Cha <chamrlsmtz@gmail.com>
2025-05-25 16:59:31 -0600

    referencia a la sql

---
+++ Model/m_database.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+public class DB {
+    protected static string connectionString = "";
+    protected SqlConnection connection;
+    protected string table = "";
+
+    public DB()
+    {
+        connection = new SqlConnection(connectionString);
+    }
+
+    public static void SetConnectionString(string server, string database, string user, string password) {
+        connectionString = $"Server={server};Database={database};User Id={user};Password={password};TrustServerCertificate=True;";
+    }
+
+    public bool Create(string columns, string values) {
+        string query = $"INSERT INTO {table} ({columns}) VALUES ({values})";
+        return ExecuteQuery(query);
+    }
+
+    public List<Dictionary<string, object?>> Read(string condition = "1=1") {
+        string query = $"SELECT * FROM {table} WHERE {condition}";
+        var result = new List<Dictionary<string, object?>>();
+
+        using (var conn = new SqlConnection(connectionString)) {
+            conn.Open();
+            using (var command = new SqlCommand(query, conn))
+            using (var reader = command.ExecuteReader()) {
+                while (reader.Read()) {
+                    var row = new Dictionary<string, object?>();
+                    for (int i = 0; i < reader.FieldCount; i++) {
+                        row[reader.GetName(i)] = reader.IsDBNull(i) ? null : reader.GetValue(i);
+                    }
+                    result.Add(row);
+                }
+            }
+        }
+        return result;
+    }
+
+    public bool Update(string setColumns, string condition) {
+        string query = $"UPDATE {table} SET {setColumns} WHERE {condition}";
+        return ExecuteQuery(query);
+    }
+
+    public bool Delete(string condition) {
+        string query = $"DELETE FROM {table} WHERE {condition}";
+        return ExecuteQuery(query);
+    }
+
+    protected bool ExecuteQuery(string query) {
+        try {
+            connection.Open();
+            using (var command = new SqlCommand(query, connection)) {
+                command.ExecuteNonQuery();
+                return true;
+            }
+        } catch (Exception ex) {
+            Console.WriteLine($"Error executing query: {ex.Message}");
+            return false;
+        } finally {
+            connection.Close();
+        }
+    }
+}
