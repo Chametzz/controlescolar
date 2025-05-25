9d825387b200ee9270591c24e1130bc7cd54dceb
Cha <chamrlsmtz@gmail.com>
2025-05-25 17:35:12 -0600

    coneccion crud

--- Model/m_database.cs
+++ Model/m_database.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 public class DB {
-    protected static string connectionString = ;
+    protected static string connectionString = SetConnectionString("localhost", "controlescolar_db", "root", "123");
     protected SqlConnection connection;
     protected string table = "";
 
