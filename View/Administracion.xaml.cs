60b886c69f1c2e79869189cdfdf7228a651c6bec
Itzel <itzelbakugou@gmail.com>
2025-05-27 14:47:29 -0600

    Ayuda, no jala por la base de datos

---
+++ View/Administracion.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace controlescolar
+{
+    /// <summary>
+    /// Lógica de interacción para Administracion.xaml
+    /// </summary>
+    public partial class Administracion : Window
+    {
+        public Administracion()
+        {
+            InitializeComponent();
+            AdmonFrame.Navigate(new PageAgEmpleado());
+        }
+
+        private void BtnAgEmpleado_Click(object sender, RoutedEventArgs e)
+        {
+            AdmonFrame.Navigate(new PageAgEmpleado());
+
+        }
+
+        private void BtnSalario_Click(object sender, RoutedEventArgs e)
+        {
+            AdmonFrame.Navigate(new PageEmpleados());
+        }
+
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
