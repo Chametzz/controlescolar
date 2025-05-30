e479f0e038df4cbea2f4249c0c7ea39ec23cac84
Jorge <12162005j@gmail.com>
2025-05-30 01:11:01 -0600

    Hola mund 2

--- SQL_SERVER/Relaciones de Consultas/Alumno_Clase.cs
+++ SQL_SERVER/Relaciones de Consultas/Alumno_Clase.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Control_Escolar_Consola.Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,56 @@ namespace controlescolar.SQL_SERVER.Relaciones_de_Consultas
 {
     class Alumno_Clase
     {
+        int Id;
+        Alumno _AlumnoDate;
+        Clase _ClaseDate;
+        DateTime _FechaInscripcion;
+
+        public int IdPropiedad
+        {
+            get => Id;
+            set => Id = value;
+        }
+
+        public Alumno AlumnoDate
+        {
+            get => _AlumnoDate;
+            set => _AlumnoDate = value;
+        }
+
+        public Clase ClaseDate
+        {
+            get => _ClaseDate;
+            set => _ClaseDate = value;
+        }
+
+        public DateTime FechaInscripcion
+        {
+            get => _FechaInscripcion;
+            set => _FechaInscripcion = value;
+        }
+    }
+
+    class Table_Alumno_Clase:connection
+    {
+        public void InsertDate(Alumno_Clase Obj)
+        {
+            using (SqlConnection sql = new SqlConnection(conexion))
+            {
+                sql.Open();
+                using (SqlCommand command = new SqlCommand(Insert, sql))
+                {
+                    command.Parameters.AddWithValue("@Id_Alumno",Obj.AlumnoDate.Id_Alumno);
+                    command.Parameters.AddWithValue("@Id_Clase", Obj.ClaseDate.Id);
+                    command.Parameters.AddWithValue("@FechaInscripcion",Obj.FechaInscripcion);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+
+
+       static string Insert = "Insert into Alumno_Clase (Id_Alumno,Id_Clase,FechaInscripcion) values (@Id_Alumno,@Id_Clase,@FechaInscripcion)";
     }
 }
