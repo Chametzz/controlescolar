87d06255a843f71da00d0e170ebc2d3180a120c5
Itzel <itzelbakugou@gmail.com>
2025-05-29 23:33:53 -0600

    Fusion ahora si viewA y viewB

---
+++ View/EstudiantePantalla.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace controlescolar
+{
+    /// <summary>
+    /// Lógica de interacción para EstudiantePantalla.xaml
+    /// </summary>
+    public partial class EstudiantePantalla : Window
+    {
+        public EstudiantePantalla()
+        {
+            InitializeComponent();
+            AlumnoFrame.Navigate(new InicioE());
+        }
+
+        private void BtnInicio_Click(object sender, RoutedEventArgs e)
+        {
+            AlumnoFrame.Navigate(new InicioE());
+        }
+
+        private void BtnEditarDatos_Click(object sender, RoutedEventArgs e)
+        {
+            AlumnoFrame.Navigate(new PageEditarDatos());
+        }
+
+        private void BtnHorario_Click(object sender, RoutedEventArgs e)
+        {
+            AlumnoFrame.Navigate(new PageHorario());
+
+        }
+        private void BtnCalificaciones_Click(object sender, RoutedEventArgs e)
+        {
+            AlumnoFrame.Navigate(new PageCalificaciones());
+
+        }
+
+        private void BtnKardex_Click(object sender, RoutedEventArgs e)
+        {
+            AlumnoFrame.Navigate(new PageKardex());
+        }
+
+        private void BtnHistoricos_Click(object sender, RoutedEventArgs e)
+        {
+            AlumnoFrame.Navigate(new PageHistoricos());
+        }
+
+        private void BtnRecibos_Click(object sender, RoutedEventArgs e)
+        {
+            AlumnoFrame.Navigate(new PageRecibos());
+        }
+
+        private void BtnCargaMateria_Click(object sender, RoutedEventArgs e)
+        {
+            AlumnoFrame.Navigate(new PageCargaMateria());
+        }
+        private void BtnTickets_Click(object sender, RoutedEventArgs e)
+        {
+            AlumnoFrame.Navigate(new PageTickets());
+        }
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Show();
+            this.Close();
+        }
+    }
+}
